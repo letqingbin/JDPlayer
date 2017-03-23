e656b84e021464bc069117de76bf24e8205a16c1
yaoshibang <yaoshibang@yaoshibangdeiMac.local>
2017-03-23 14:36:29 +0800

    Initial Commit

---
+++ JDPlayerPro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JDPlayerPro
+//
+//  Created by yaoshibang on 2017/3/23.
+//  Copyright © 2017年 depa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JDPlayerPro/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JDPlayerPro
+//
+//  Created by yaoshibang on 2017/3/23.
+//  Copyright © 2017年 depa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
